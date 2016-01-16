105f34069f5e94865294a7d269d188a1e5f392d7
BeMg <gccbg04538@gmail.com>
2016-01-16 14:59:39 +0800

    add UVa 1583 AC

---
+++ UVa/1583/main.cc
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+int generator(int m){
+    int ans=0;
+    ans = m;
+    while(m!=0){
+	ans+=m%10;
+	m/=10;
+    }
+    return ans;
+}
+
+//Create table for not TLE.
+int table[100000+500]={};
+
+
+int main(){
+    int n;
+    //Get ans for all range.
+    for(int i=100000; i>=0; i--){
+	table[generator(i)] = i;
+    }
+    while(scanf("%d",&n)!=EOF){
+	while(n--){
+	    int m;
+	    scanf("%d",&m);
+	    printf("%d\n",table[m]);
+	}
+    }
+    return 0;
+}
